46f61885111d63184aef9f81edcb6be85dce990a
Fernando Salazar <fesarguedas@gmail.com>
2025-07-28 16:58:10 -0600

    Fix
    Modified client and consumer to match topic

--- Proyecto_Final/bots/client_1.py
+++ Proyecto_Final/bots/client_1.py
@@ -4,24 +4,23 @@ import pandas as pd
 import time
 import random
 
-data = pd.read_csv('Proyecto_Final/producers/data.csv', encoding='windows-1252')
+data = pd.read_csv('Proyecto_Final/bots/data.csv', encoding='windows-1252')
 
 comments = data.iloc[:9160, :2].to_dict(orient='records')
-print('Comments loaded:', len(comments))
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092',
                          value_serializer=lambda v: json.dumps(v).encode('utf-8')
                          )
 
-
 while True:
     try:
         comment = random.choice(comments)
-        key = str(comment['id']).encode('utf-8')
-        value = str(comment['comment']).encode('utf-8')
-        producer.send('comments', key=key, value=value)
+        key = str(comment['textID']).encode('utf-8')
+        value = str(comment['text']).encode('utf-8')
+        producer.send('user-topic', value=comment) #Muy importante que el topic sea el mismo que el del consumer
         time.sleep(random.uniform(0.5, 3.0))
-
+        print(f"Sent: {comment}")
+        print(f'Time between messages: {random.uniform(0.5, 3.0):.2f} seconds')
     except Exception as e:
         print("Error occurred:", e)
 
---
+++ Proyecto_Final/bots/client_3.py
@@ -0,0 +1,26 @@
+from kafka import KafkaProducer
+import pandas as pd
+import time
+import random
+
+data = pd.read_csv('Proyecto_Final/producers/data.csv', encoding='windows-1252')
+
+comments = data.iloc[18321:27481, :2].to_dict(orient='records')
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092',
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8')
+                         )
+
+while True:
+    try:
+        comment = random.choice(comments)
+        key = str(comment['textID']).encode('utf-8')
+        value = str(comment['text']).encode('utf-8')
+        producer.send('user-topic', value=comment) #Muy importante que el topic sea el mismo que el del consumer
+        time.sleep(random.uniform(0.5, 3.0))
+        print(f"Sent: {comment}")
+    except Exception as e:
+        print("Error occurred:", e)
+
+producer.flush()
+producer.close()
